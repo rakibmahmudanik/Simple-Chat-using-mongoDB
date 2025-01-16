f49e2f5789160b015f1f18f8930146a62d56505c
RAKIB MAHMUD <77047747+rakibmahmudanik@users.noreply.github.com>
2025-01-16 16:59:53 +0600

    Create Edit function

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
+
 const Chat = require("./models/chats.js");
 const app = express();
 
@@ -8,6 +10,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 main()
   .then(() => {
@@ -41,6 +44,34 @@ app.post("/", (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+  res.redirect("/");
+});
+
+app.get("/edit/:id", async (req, res) => {
+  const chats = await Chat.find();
+  let { id } = req.params;
+  let singleChat = await Chat.findById(id);
+  res.render("edit.ejs", { chats, singleChat });
+});
+
+app.patch("/:id", async (req, res) => {
+  let { id } = req.params;
+  let { form: newFrom, to: newTo, msg: newMsg } = req.body;
+  let updateChat = await Chat.findByIdAndUpdate(
+    id,
+    {
+      from: newFrom,
+      to: newTo,
+      msg: newMsg,
+    },
+    {
+      runValidators: true,
+      new: true,
+    }
+  );
+
+  console.log(updateChat);
+
   res.redirect("/");
 });
 
